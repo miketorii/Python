6899339c32d6e74a35123e7398b7717b4f776c6a
miketorii <miketorii@gmail.com>
2023-01-15 01:58:16 +0900

    Calculation of matrix

---
+++ numpy/matrix.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+arr = np.arange(6)
+print(arr)
+
+arr21 = np.arange(4).reshape((2,2))
+print(arr21)
+
+arr21_inv = np.linalg.inv(arr21)
+print(arr21_inv)
+
+#
+arr31 = np.arange(6).reshape((2,3))
+print(arr31)
+print(arr31.T)
+# 0 1 2
+# 3 4 5
+mat31 = np.matrix(arr31)
+#print(mat31)
+
+print('----------')
+b = np.random.randint(-10, 10, size=(3,3)) 
+print(b)
+c = np.linalg.inv(b)
+print(c)
+print('----------')
+
+#
+arr32 = np.arange(0,12,2).reshape((2,3))
+#print(arr32)
+# 0 2 4
+# 6 8 10
+mat32 = np.matrix(arr32)
+#print(mat32)
+
+#ans = np.dot(arr21, arr31)
+#print(ans)
+
+#ans = arr21 @ arr31
+#print(ans)
+
+ans = np.matmul(arr21, arr31)
+print(ans)
+
+ans = np.matmul(arr31, arr32.T)
+print(ans)
+
+
+
